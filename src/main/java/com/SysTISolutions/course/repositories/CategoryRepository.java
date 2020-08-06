5c7eb1615ccdcaae4ab843ec801f0ed3c9d197b2
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-08-06 10:48:35 -0300

    Category

---
+++ src/main/java/com/SysTISolutions/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.SysTISolutions.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.SysTISolutions.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
