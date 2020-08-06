1ede8da1c0565dd49e75e79c7eb39510825c82f6
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-08-06 15:19:26 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/SysTISolutions/course/config/TestConfig.java
+++ src/main/java/com/SysTISolutions/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.SysTISolutions.course.entities.Category;
 import com.SysTISolutions.course.entities.Order;
+import com.SysTISolutions.course.entities.OrderItem;
 import com.SysTISolutions.course.entities.Product;
 import com.SysTISolutions.course.entities.User;
 import com.SysTISolutions.course.entities.enums.OrderStatus;
 import com.SysTISolutions.course.repositories.CategoryRepository;
+import com.SysTISolutions.course.repositories.OrderItemRepository;
 import com.SysTISolutions.course.repositories.OrderRepository;
 import com.SysTISolutions.course.repositories.ProductRepository;
 import com.SysTISolutions.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired 
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,6 +77,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p2, 1, p2.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
---
+++ src/main/java/com/SysTISolutions/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.SysTISolutions.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.SysTISolutions.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
