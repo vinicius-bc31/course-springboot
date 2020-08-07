d839be56dcb62dce695d3dfb96df857af50fbb2b
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-08-07 11:54:15 -0300

    Exception handling - delete

--- src/main/java/com/SysTISolutions/course/services/UserService.java
+++ src/main/java/com/SysTISolutions/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.SysTISolutions.course.entities.User;
 import com.SysTISolutions.course.repositories.UserRepository;
+import com.SysTISolutions.course.services.exceptions.DatabaseException;
 import com.SysTISolutions.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,10 +33,16 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
-	public User update( Long id,User obj) {
+
+	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
@@ -43,6 +52,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
